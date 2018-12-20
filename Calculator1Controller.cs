50911d5d150208296ca62d6a6357558897018a54
imssr1 <31723946+imssr1@users.noreply.github.com>
2018-12-19 21:59:21 -0800

    Add files via upload

---
+++ Calculator1Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace watertank.Controllers
+{
+    public class Calculator1Controller : Controller
+    {
+        // GET: Calculator1
+        [HttpGet]
+        public ActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculator(int? num1, int? num2, string command)
+        {
+            if (command == "add")
+            {
+                int? sum = num1 + num2;
+                ViewBag.sum = sum;
+            }
+            if (command == "sub")
+            {
+                int? sum = num1 - num2;
+                ViewBag.sum = sum;
+            }
+            if (command == "mul")
+            {
+                int? sum = num1 * num2;
+                ViewBag.sum = sum;
+            }
+            if (command == "div")
+            {
+                int? sum = num1 / num2;
+                ViewBag.sum = sum;
+            }
+
+            ViewBag.num1 = num1;
+            ViewBag.num2 = num2;
+            
+            return View();
+        }
+    }
+}
